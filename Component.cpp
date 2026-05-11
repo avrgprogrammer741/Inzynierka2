d844cbbb7cc8801ff3b71879a297622c8864d8e5
avrgprogrammer741 <88510480+avrgprogrammer741@users.noreply.github.com>
2026-05-11 20:23:52 +0200

    Add files via upload

--- Component.cpp
+++ Component.cpp
@@ -83,23 +83,14 @@ Component::Component(std::string name, int version, std::optional<std::string> o
 }
 
 void Component::interpretCommand(const std::string& command) {
+        FILE* f = fmemopen(
+            (void*)command.c_str(),
+            command.size(),
+            "r"
+        );
 
-    const char* filepath = "C:\\Users\\user\\Desktop\\forth_temp.fs";
-
-    FILE* f = fopen(filepath, "w");
     if (!f) {
-        throw std::runtime_error("Error cannot create script");
-    }
-
-    if (fprintf(f, "%s", command.c_str()) < 0) {
-        fclose(f);
-        throw std::runtime_error("Error cannot write to script file");
-    }
-    fclose(f);
-
-    f = fopen(filepath, "r");
-    if (!f) {
-        throw std::runtime_error("Error cannot open script file");
+        throw std::runtime_error("Error creating memory stream");
     }
 
     forthState->input = f;
